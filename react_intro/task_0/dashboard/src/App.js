9d196deea8454a89443cac61a320d99a874d516d
misaelbazan <misaelbfranco@gmail.com>
2024-05-28 11:29:11 -0500

    Adding src/

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,28 +1,26 @@
-import logo from './logo.jpg';
+import logo from './holberton_logo.jpg';
 import './App.css';
 
 function App() {
   return (
-    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="Header-Title">School dashboard</h1>
-      </header>
-      <hr className='Div-Separator'/>
-      <body className="App-body">
-        <p>
-          Login to access the full dashboard
-        </p>
-      </body>
-      <hr className='Div-Separator'/>
-      <footer className="App-footer">
-        <p>
-          Copyright 2024 - holberton School
-        </p>
-      </footer>
-      </div>
-    </>
+      	<header className="App-header">
+        	<img src={logo} className="App-logo" alt="logo" />
+        	<h1>
+			School dashboard	         
+        	</h1>
+      	</header>
+		<div className="App-separator"></div>
+		<body className="App-body">
+			<p>
+				Login to acces the full dashboard
+			</p>
+		</body>
+		<div className="App-separator"></div>
+		<footer className="App-footer">
+			<p>Copyright 2024 - holberton School</p>
+		</footer>	
+    </div>
   );
 }
 
