ba8c503611b393a7fb944d2dc4787a4486438bd0
misaelbazan <misaelbfranco@gmail.com>
2024-05-28 23:08:51 -0500

    src/ added

--- react_intro/task_4/src/App.js
+++ react_intro/task_4/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import logo from './holberton_logo.jpg';
 import './App.css';
+import { getFooterCopy, getFullYear} from './utils'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      	<header className="App-header">
+        	<img src={logo} className="App-logo" alt="logo" />
+        	<h1>
+			School dashboard	         
+        	</h1>
+      	</header>
+		<div className="App-separator"></div>
+		<div className="App-body">
+			<p>
+				Login to access the full dashboard
+			</p>
+			<form>
+				<label htmlFor='email'>Email:</label>
+				<input type='email' id='email'></input>
+				<label htmlFor='pass'>Password:</label>
+				<input type='password' id='pass'></input>
+				<button type='button'>OK</button>
+			</form>
+		</div>
+		<div className="App-separator"></div>
+		<footer className="App-footer">
+			<p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+		</footer>	
     </div>
   );
 }
--- react_intro/task_4/src/App.test.js
+++ react_intro/task_4/src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+    let app_wrapper;
+
+    beforeEach(() => {
+        app_wrapper = shallow(<App />);
+    });
+
+    it('Renders <App /> without breaking', () => {
+        expect(app_wrapper.exists()).toBe(true);
+    });
+
+    it('Renders <App /> and find .App-header component', () => {
+        expect(app_wrapper.find('.App-header').length).toBe(1);
+    });
+
+    it('Renders <App /> and find .App-body component', () => {
+        expect(app_wrapper.find('.App-body').length).toBe(1);
+    });
+
+    it('Renders <App /> and find .App-footer component', () => {
+        expect(app_wrapper.find('.App-footer').length).toBe(1);
+    });
 });
