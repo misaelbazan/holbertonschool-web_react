ee22de2b84356b9167b914e22684bbbf1e397d9d
misaelbazan <misaelbfranco@gmail.com>
2024-05-29 10:49:43 -0500

    Adding all reorganized files

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -30,6 +30,11 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
