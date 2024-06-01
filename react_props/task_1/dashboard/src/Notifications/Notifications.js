f7be39904940d34cdc7433ec17ba8e1257bb5849
misaelbazan <misaelbfranco@gmail.com>
2024-06-01 07:45:40 -0500

    Test added

---
+++ react_props/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils';
+
+function Notifications() {
+    return (
+        <div className='Notifications'>
+            <p>
+                Here is the list of notifications
+            </p>
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li
+                    data-priority="urgent"
+                    dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+                >
+                </li>
+            </ul>
+            <button
+                style={{
+                    position: 'relative',
+                    left: '98%',
+                    bottom: '8.5rem',
+                    border: 'none',
+                    background: 'transparent',
+                    cursor: 'pointer',
+                    fontSize: '16px',
+                }}
+                aria-label='Close'
+                onClick={() => console.log('Close button has been clicked')}
+            >x</button>
+        </div>
+    );
+};
+
+export default Notifications;
